a48e106a1ca07bdc97e4a7a226f15c610054e035
josuecedeno <josuecedeno@yahoo.com>
2020-11-11 11:31:27 -0600

    Delete Profile and User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,24 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route     DELETE api/profile/
+// @desc      Delete profiles, user, and posts
+// @access    Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - Remove users posts
+
+    // Remove Profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
