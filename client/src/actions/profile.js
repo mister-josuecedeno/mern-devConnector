34bcfc5c9aa08cb93a04cfa7bed83650370095da
josuecedeno <josuecedeno@yahoo.com>
2020-11-28 12:16:44 -0600

    Displaying Github Repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -67,7 +67,7 @@ export const getProfileById = (userId) => async (dispatch) => {
 // Get Github Repos
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/${username}`);
+    const res = await axios.get(`/api/profile/github/${username}`);
 
     dispatch({
       type: GET_REPOS,
